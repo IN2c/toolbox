a6beae6b231b5c4a353c9a17554d7fcf47eccddb
Oliver Füllemann <oliver_10@hotmail.ch>
2016-03-21 10:28:56 +0100

    CelsiusInFahrenheit fertig

--- toolbox/src/toolbox/TestClass.java
+++ toolbox/src/toolbox/TestClass.java
@@ -4,6 +4,9 @@ public class TestClass {
 
 	public static void main(String[] args) 
 	{
+		Umwandlung Umwandlung = new Umwandlung();
+		Umwandlung.CelsiusInFahrenheit(32);
 		
+		Umwandlung.CelsiusInFahrenheit(10);
 	}
 }
--- toolbox/src/toolbox/Umwandlung.java
+++ toolbox/src/toolbox/Umwandlung.java
@@ -1,10 +1,13 @@
 package toolbox;
 
+
 public class Umwandlung {
 	
-	public void CelsiusInFahrenheit()
+	public void CelsiusInFahrenheit(double celsius)  //Oliver
 	{
-		
+		double fahrenheit = 0;
+		fahrenheit = celsius * 1.8 + 32;
+		System.out.printf("%4.2f Grad Celsius entsprechen in Fahrenheit: %4.2f\n\n\n", celsius, fahrenheit);
 	}
 	
 	public void FahrenheitInCelsius()
@@ -16,4 +19,12 @@ public class Umwandlung {
 		
 	}
 	
+	public void EuroInFranken()
+	{
+		
+	}
+	public void MBinGB()
+	{
+		
+	}
 }
