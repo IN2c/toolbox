4387d05be606abf282f40181d4c76577b5c7a472
Toubi01 <tobias.verstappen@bluewin.ch>
2016-04-11 15:41:41 +0200

    autopreis

--- toolbox/src/toolbox/TestClass.java
+++ toolbox/src/toolbox/TestClass.java
@@ -19,7 +19,7 @@ public class TestClass {
 		Umwandlung gbtomb = new Umwandlung();
 		gbtomb.GBinMB(125.3);
 		
-		AutopreisBerechnen auto = new AutopreisBerechnen(8000.0, 3, 333, 4000.0);
+		AutopreisBerechnen auto = new AutopreisBerechnen(8000.0, 4, 333, 4000.0);
 		auto.Berechnung();
 
 		Berechnungen zufallszahl1= new Berechnungen();
